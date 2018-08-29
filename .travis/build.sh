e83a5f7cfa39e712eec9cdcc4fabd3b8dac73a2a
Tom Bentley <tbentley@redhat.com>
2018-08-29 14:53:51 +0100

    Travis build

---
+++ .travis/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+export PULL_REQUEST=${PULL_REQUEST:-true}
+export BRANCH=${BRANCH:-master}
+export TAG=${TAG:-latest}
+export DOCKER_ORG=${DOCKER_ORG:-strimzici}
+export DOCKER_REGISTRY=${DOCKER_REGISTRY:-docker.io}
+export DOCKER_TAG=$COMMIT
+
+make java_build docker_build
+
+if [ "$PULL_REQUEST" != "false" ] ; then
+    echo "Building PR: Nothing to push"
+else
+  docker login -u $DOCKER_USER -p $DOCKER_PASS
+  if [ "$TAG" = "latest" ] && [ "$BRANCH" != "master" ]; then
+    export DOCKER_ORG=strimzici
+    export DOCKER_TAG=$TAG
+  else
+    export DOCKER_ORG=strimzi
+    export DOCKER_TAG=$TAG
+  fi
+  make docker_push
+fi
