70f682bb7118780c4ad582f0344cfb1e90ce6bad
Jakub Scholz <www@scholzj.com>
2021-03-25 14:11:51 +0100

    Prepare build to rename master branch to main (#56)
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 export PULL_REQUEST=${PULL_REQUEST:-true}
-export BRANCH=${BRANCH:-master}
+export BRANCH=${BRANCH:-main}
 export TAG=${TAG:-latest}
 export DOCKER_ORG=${DOCKER_ORG:-strimzici}
 export DOCKER_REGISTRY=${DOCKER_REGISTRY:-docker.io}
@@ -15,14 +15,14 @@ if [ "$PULL_REQUEST" != "false" ] ; then
   make docker_build
   echo "Building PR: Nothing to push"
 else
-  if [ "$TAG" = "latest" ] && [ "$BRANCH" != "master" ]; then
+  if [ "$TAG" = "latest" ] && [ "$BRANCH" != "main" ]; then
     make docker_build
-    echo "Not in tag or master branch: Nothing to push"
+    echo "Not in tag or main branch: Nothing to push"
   else
     export DOCKER_TAG=$TAG
     make docker_build
 
-    echo "In tag or master branch: Pushing images"
+    echo "In tag or main branch: Pushing images"
     docker login -u $QUAY_USER -p $QUAY_PASS $DOCKER_REGISTRY
     make docker_push
   fi
