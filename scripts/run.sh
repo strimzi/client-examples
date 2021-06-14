80590b83c9434e24427ccac282a90af70a63299c
Jakub Scholz <www@scholzj.com>
2021-06-14 21:50:51 +0200

    Update the OAuth library to 0.8.0 (#61)
    * Update the OAuth library to 0.8.0
    
    Signed-off-by: Jakub Scholz <www@scholzj.com>
    
    * Update to final 0.8.0 release
    
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
 set +x
 
-# Parameters:
-# $1: Path to the new truststore
-# $2: Truststore password
-# $3: Public key to be imported
-# $4: Alias of the certificate
-function create_truststore {
-   keytool -keystore $1 -storepass $2 -noprompt -alias $4 -import -file $3 -storetype PKCS12
-}
-
 if [ -z "$JAVA_OPTS" ]; then
     export JAVA_OPTS="${JAVA_OPTS} -Dlog4j2.configurationFile=file:bin/log4j2.properties"
-fi
-
-if [ "$OAUTH_CRT" ];
-then
-    echo "Preparing OAuth truststore"
-    export OAUTH_SSL_TRUSTSTORE_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
-    echo "$OAUTH_CRT" > /tmp/oauth.crt
-    create_truststore /tmp/oauth-truststore.p12 $OAUTH_SSL_TRUSTSTORE_PASSWORD /tmp/oauth.crt ca
-    export OAUTH_SSL_TRUSTSTORE_LOCATION=/tmp/oauth-truststore.p12
 fi
 
 # Make sure that we use /dev/urandom
