efba0ded1030af2811de6dbe4ff9b1082bab3186
Owen <owencorrigan76@gmail.com>
2022-10-20 16:14:22 +0100

    fix nullPointerException error
    Signed-off-by: Owen <owencorrigan76@gmail.com>

--- java/kafka/consumer/src/main/java/KafkaConsumerConfig.java
+++ java/kafka/consumer/src/main/java/KafkaConsumerConfig.java
@@ -72,7 +72,7 @@ public class KafkaConsumerConfig {
         String oauthRefreshToken = System.getenv("OAUTH_REFRESH_TOKEN");
         String oauthTokenEndpointUri = System.getenv("OAUTH_TOKEN_ENDPOINT_URI");
         String additionalConfig = System.getenv().getOrDefault("ADDITIONAL_CONFIG", "");
-        TracingSystem tracingSystem = TracingSystem.forValue(System.getenv("TRACING_SYSTEM"));
+        TracingSystem tracingSystem = TracingSystem.forValue(System.getenv().getOrDefault("TRACING_SYSTEM", ""));
 
         return new KafkaConsumerConfig(bootstrapServers, topic, groupId, clientRack, messageCount, sslTruststoreCertificates, sslKeystoreKey,
                 sslKeystoreCertificateChain, oauthClientId, oauthClientSecret, oauthAccessToken, oauthRefreshToken, oauthTokenEndpointUri,
