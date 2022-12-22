03623088893dc340e5b350753947a27a2deca64a
Owen Corrigan <76431349+OwenCorrigan76@users.noreply.github.com>
2022-12-22 15:06:16 +0000

    Refactor Client-Example ConsumerConfigs (#91)
    * for refactoring
    
    Signed-off-by: Owen <owencorrigan76@gmail.com>
    
    * refactored ConsumerConfigs
    
    Signed-off-by: Owen <owencorrigan76@gmail.com>
    
    * refactored ConsumerConfigs
    
    Signed-off-by: Owen <owencorrigan76@gmail.com>
    
    * removed Ssl and Oauth configs
    
    Signed-off-by: Owen <owencorrigan76@gmail.com>
    
    * Addressing Comments
    
    Signed-off-by: Owen <owencorrigan76@gmail.com>
    
    * Indentation in jaeger.yaml
    
    Signed-off-by: Owen <owencorrigan76@gmail.com>
    
    * Addressing Kyle's Comments
    
    Signed-off-by: Owen <owencorrigan76@gmail.com>
    
    * Addressing Jakub's Comments and changing Repo name
    
    Signed-off-by: Owen <owencorrigan76@gmail.com>
    
    * Addressing Paolo's Comments
    
    Signed-off-by: Owen <owencorrigan76@gmail.com>
    
    * Addressing Jakub and Paolo's Comments
    
    Signed-off-by: Owen <owencorrigan76@gmail.com>
    
    * Further Addressing Jakub's Comments
    
    Signed-off-by: Owen <owencorrigan76@gmail.com>
    
    Signed-off-by: Owen <owencorrigan76@gmail.com>

--- java/kafka/consumer/src/main/java/KafkaConsumerConfig.java
+++ java/kafka/consumer/src/main/java/KafkaConsumerConfig.java
@@ -3,228 +3,56 @@
  * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
  */
 
-import org.apache.kafka.clients.CommonClientConfigs;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.config.SaslConfigs;
-import org.apache.kafka.common.config.SslConfigs;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
+import java.util.Map;
 import java.util.Properties;
-import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 public class KafkaConsumerConfig {
-    private static final Logger log = LogManager.getLogger(KafkaConsumerConfig.class);
-
     private static final long DEFAULT_MESSAGES_COUNT = 10;
-    private final String bootstrapServers;
-    private final String topic;
-    private final String groupId;
-    private final String autoOffsetReset = "earliest";
-    private final String enableAutoCommit = "false";
-    private final String clientRack;
-    private final Long messageCount;
-    private final String sslTruststoreCertificates;
-    private final String sslKeystoreKey;
-    private final String sslKeystoreCertificateChain;
-    private final String oauthClientId;
-    private final String oauthClientSecret;
-    private final String oauthAccessToken;
-    private final String oauthRefreshToken;
-    private final String oauthTokenEndpointUri;
-    private final String additionalConfig;
-    private final String saslLoginCallbackClass = "io.strimzi.kafka.oauth.client.JaasClientOauthLoginCallbackHandler";
-    private final TracingSystem tracingSystem;
+    private static final String KAFKA_PREFIX = "KAFKA_";
 
-    public KafkaConsumerConfig(String bootstrapServers, String topic, String groupId, String clientRack, Long messageCount,
-                               String sslTruststoreCertificates, String sslKeystoreKey, String sslKeystoreCertificateChain,
-                               String oauthClientId, String oauthClientSecret, String oauthAccessToken, String oauthRefreshToken,
-                               String oauthTokenEndpointUri, String additionalConfig, TracingSystem tracingSystem) {
-        this.bootstrapServers = bootstrapServers;
+    private final String topic;
+    private final Long messageCount;
+    private final TracingSystem tracingSystem;
+    private final Properties properties;
+
+    public KafkaConsumerConfig(String topic, Long messageCount, TracingSystem tracingSystem, Properties properties) {
         this.topic = topic;
-        this.groupId = groupId;
-        this.clientRack = clientRack;
         this.messageCount = messageCount;
-        this.sslTruststoreCertificates = sslTruststoreCertificates;
-        this.sslKeystoreKey = sslKeystoreKey;
-        this.sslKeystoreCertificateChain = sslKeystoreCertificateChain;
-        this.oauthClientId = oauthClientId;
-        this.oauthClientSecret = oauthClientSecret;
-        this.oauthAccessToken = oauthAccessToken;
-        this.oauthRefreshToken = oauthRefreshToken;
-        this.oauthTokenEndpointUri = oauthTokenEndpointUri;
-        this.additionalConfig = additionalConfig;
         this.tracingSystem = tracingSystem;
+        this.properties = properties;
     }
 
     public static KafkaConsumerConfig fromEnv() {
-        String bootstrapServers = System.getenv("BOOTSTRAP_SERVERS");
-        String topic = System.getenv("TOPIC");
-        String groupId = System.getenv("GROUP_ID");
-        String clientRack = System.getenv("CLIENT_RACK");
-        Long messageCount = System.getenv("MESSAGE_COUNT") == null ? DEFAULT_MESSAGES_COUNT : Long.parseLong(System.getenv("MESSAGE_COUNT"));
-        String sslTruststoreCertificates = System.getenv("CA_CRT");
-        String sslKeystoreKey = System.getenv("USER_KEY");
-        String sslKeystoreCertificateChain = System.getenv("USER_CRT");
-        String oauthClientId = System.getenv("OAUTH_CLIENT_ID");
-        String oauthClientSecret = System.getenv("OAUTH_CLIENT_SECRET");
-        String oauthAccessToken = System.getenv("OAUTH_ACCESS_TOKEN");
-        String oauthRefreshToken = System.getenv("OAUTH_REFRESH_TOKEN");
-        String oauthTokenEndpointUri = System.getenv("OAUTH_TOKEN_ENDPOINT_URI");
-        String additionalConfig = System.getenv().getOrDefault("ADDITIONAL_CONFIG", "");
-        TracingSystem tracingSystem = TracingSystem.forValue(System.getenv().getOrDefault("TRACING_SYSTEM", ""));
-
-        return new KafkaConsumerConfig(bootstrapServers, topic, groupId, clientRack, messageCount, sslTruststoreCertificates, sslKeystoreKey,
-                sslKeystoreCertificateChain, oauthClientId, oauthClientSecret, oauthAccessToken, oauthRefreshToken, oauthTokenEndpointUri,
-                additionalConfig, tracingSystem);
+        String topic = System.getenv("STRIMZI_TOPIC");
+        Long messageCount = System.getenv("STRIMZI_MESSAGE_COUNT") == null ? DEFAULT_MESSAGES_COUNT : Long.parseLong(System.getenv("STRIMZI_MESSAGE_COUNT"));
+        TracingSystem tracingSystem = TracingSystem.forValue(System.getenv().getOrDefault("STRIMZI_TRACING_SYSTEM", ""));
+        Properties properties = new Properties();
+        properties.putAll(System.getenv()
+                .entrySet()
+                .stream()
+                .filter(mapEntry -> mapEntry.getKey().startsWith(KAFKA_PREFIX))
+                .collect(Collectors.toMap(mapEntry -> convertEnvVarToPropertyKey(mapEntry.getKey()), Map.Entry::getValue)));
+        return new KafkaConsumerConfig(topic, messageCount, tracingSystem, properties);
     }
 
-    public static Properties createProperties(KafkaConsumerConfig config) {
-        Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, config.getBootstrapServers());
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, config.getGroupId());
-        if (config.getClientRack() != null) {
-            props.put(ConsumerConfig.CLIENT_RACK_CONFIG, config.getClientRack());
-        }
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, config.getAutoOffsetReset());
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, config.getEnableAutoCommit());
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-
-        if (config.getSslTruststoreCertificates() != null)   {
-            log.info("Configuring truststore");
-            props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
-            props.put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, "PEM");
-            props.put(SslConfigs.SSL_TRUSTSTORE_CERTIFICATES_CONFIG, config.getSslTruststoreCertificates());
-        }
-
-        if (config.getSslKeystoreCertificateChain() != null && config.getSslKeystoreKey() != null)   {
-            log.info("Configuring keystore");
-            props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
-            props.put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, "PEM");
-            props.put(SslConfigs.SSL_KEYSTORE_CERTIFICATE_CHAIN_CONFIG, config.getSslKeystoreCertificateChain());
-            props.put(SslConfigs.SSL_KEYSTORE_KEY_CONFIG, config.getSslKeystoreKey());
-        }
-
-        Properties additionalProps = new Properties();
-        if (!config.getAdditionalConfig().isEmpty()) {
-            StringTokenizer tok = new StringTokenizer(config.getAdditionalConfig(), System.lineSeparator());
-            while (tok.hasMoreTokens()) {
-                String record = tok.nextToken();
-                int endIndex = record.indexOf('=');
-                if (endIndex == -1) {
-                    throw new RuntimeException("Failed to parse Map from String");
-                }
-                String key = record.substring(0, endIndex);
-                String value = record.substring(endIndex + 1);
-                additionalProps.put(key.trim(), value.trim());
-            }
-        }
-
-        if ((config.getOauthAccessToken() != null)
-                || (config.getOauthTokenEndpointUri() != null && config.getOauthClientId() != null && config.getOauthRefreshToken() != null)
-                || (config.getOauthTokenEndpointUri() != null && config.getOauthClientId() != null && config.getOauthClientSecret() != null))    {
-            log.info("Configuring OAuth");
-            props.put(SaslConfigs.SASL_JAAS_CONFIG, "org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required;");
-            props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL".equals(props.getProperty(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG)) ? "SASL_SSL" : "SASL_PLAINTEXT");
-            props.put(SaslConfigs.SASL_MECHANISM, "OAUTHBEARER");
-            if (!(additionalProps.containsKey(SaslConfigs.SASL_MECHANISM) && additionalProps.getProperty(SaslConfigs.SASL_MECHANISM).equals("PLAIN"))) {
-                props.put(SaslConfigs.SASL_LOGIN_CALLBACK_HANDLER_CLASS, config.saslLoginCallbackClass);
-            }
-        }
-
-        // override properties with defined additional properties
-        props.putAll(additionalProps);
-
-        return props;
-    }
-
-    public String getBootstrapServers() {
-        return bootstrapServers;
+    private static String convertEnvVarToPropertyKey(String envVar) {
+        return envVar.substring(envVar.indexOf("_") + 1).toLowerCase().replace("_", ".");
     }
 
     public String getTopic() {
         return topic;
     }
 
-    public String getGroupId() {
-        return groupId;
-    }
-
-    public String getAutoOffsetReset() {
-        return autoOffsetReset;
-    }
-
-    public String getEnableAutoCommit() {
-        return enableAutoCommit;
-    }
-
-    public String getClientRack() {
-        return clientRack;
-    }
-
     public Long getMessageCount() {
         return messageCount;
     }
 
-    public String getSslTruststoreCertificates() {
-        return sslTruststoreCertificates;
+    public TracingSystem getTracingSystem() {
+        return tracingSystem;
     }
 
-    public String getSslKeystoreKey() {
-        return sslKeystoreKey;
-    }
-
-    public String getSslKeystoreCertificateChain() {
-        return sslKeystoreCertificateChain;
-    }
-
-    public String getOauthClientId() {
-        return oauthClientId;
-    }
-
-    public String getOauthClientSecret() {
-        return oauthClientSecret;
-    }
-
-    public String getOauthAccessToken() {
-        return oauthAccessToken;
-    }
-
-    public String getOauthRefreshToken() {
-        return oauthRefreshToken;
-    }
-
-    public String getOauthTokenEndpointUri() {
-        return oauthTokenEndpointUri;
-    }
-
-    public String getAdditionalConfig() {
-        return additionalConfig;
-    }
-
-    public TracingSystem getTracingSystem() { return tracingSystem; }
-
-    @Override
-    public String toString() {
-        return "KafkaConsumerConfig{" +
-            "bootstrapServers='" + bootstrapServers + '\'' +
-            ", topic='" + topic + '\'' +
-            ", groupId='" + groupId + '\'' +
-            ", autoOffsetReset='" + autoOffsetReset + '\'' +
-            ", enableAutoCommit='" + enableAutoCommit + '\'' +
-            ", clientRack='" + clientRack + '\'' +
-            ", messageCount=" + messageCount +
-            ", sslTruststoreCertificates='" + sslTruststoreCertificates + '\'' +
-            ", sslKeystoreKey='" + sslKeystoreKey + '\'' +
-            ", sslKeystoreCertificateChain='" + sslKeystoreCertificateChain + '\'' +
-            ", oauthClientId='" + oauthClientId + '\'' +
-            ", oauthClientSecret='" + oauthClientSecret + '\'' +
-            ", oauthAccessToken='" + oauthAccessToken + '\'' +
-            ", oauthRefreshToken='" + oauthRefreshToken + '\'' +
-            ", oauthTokenEndpointUri='" + oauthTokenEndpointUri + '\'' +
-            ", additionalConfig='" + additionalConfig + '\'' +
-            ", tracingSystem='" + tracingSystem + '\'' +
-            '}';
+    public Properties getProperties() {
+        return properties;
     }
 }
