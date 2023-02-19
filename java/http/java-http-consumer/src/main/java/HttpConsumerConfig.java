84275cf4bb04d891147c5e971e64478fbcdcb099
Paolo Patierno <ppatierno@live.com>
2023-02-19 13:10:04 +0100

    Add Java SDK HTTP client examples (#105)
    * Added initial projects for Java SDK HTTP client examples
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    * Added Java SDK HTTP client producer example
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    * Added HTTP Java SDK consumer with creation, subscribe and deletion
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    * Added draft of Java SDK HTTP consumer
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    * Added Dockerfile and Makefile to Java HTTP examples
    Updated root Makefile to build Java HTTP examples
    Added default delay ms configuration to Java HTTP producer
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    * Added OpenTelemetry support on the Java SDK HTTP producer and consumer
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    * Added YAMLs to deploy Java SDK HTTP client examples to Kubernetes
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    * Updated consumer endpoint
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    * Fixed env vars on the Java SDK HTTP examples deployments
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    * Used the scheduler executor at fixed rate
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    * Added check on created consumer base_uri
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    * Fixed unused/used undeclared dependencies
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    * Updated Java installation in Dockerfile(s)
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    ---------
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

---
+++ java/http/java-http-consumer/src/main/java/HttpConsumerConfig.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+public class HttpConsumerConfig {
+
+    private static final String DEFAULT_HOSTNAME = "localhost";
+    private static final int DEFAULT_PORT = 8080;
+    private static final String DEFAULT_TOPIC = "my-topic";
+    private static final String DEFAULT_GROUPID = "my-group";
+    private static final String DEFAULT_CLIENTID = "my-consumer";
+
+    private static final long DEFAULT_MESSAGES_COUNT = 10;
+    private static final int DEFAULT_POLL_INTERVAL = 1000;
+    private static final int DEFAULT_POLL_TIMEOUT = 100;
+
+    private final String hostName;
+    private final int port;
+    private final String topic;
+    private final String groupId;
+    private final String clientId;
+    private final long messageCount;
+    private final int pollInterval;
+    private final int pollTimeout;
+    private final TracingSystem tracingSystem;
+
+    private HttpConsumerConfig(String hostName, int port, String topic, String groupId, String clientId,
+                               long messageCount, int pollInterval, int pollTimeout, TracingSystem tracingSystem) {
+        this.hostName = hostName;
+        this.port = port;
+        this.topic = topic;
+        this.groupId = groupId;
+        this.clientId = clientId;
+        this.messageCount = messageCount;
+        this.pollInterval = pollInterval;
+        this.pollTimeout = pollTimeout;
+        this.tracingSystem = tracingSystem;
+    }
+
+    public static HttpConsumerConfig fromEnv() {
+        String hostName = System.getenv("STRIMZI_HOSTNAME") == null ? DEFAULT_HOSTNAME : System.getenv("STRIMZI_HOSTNAME");
+        int port = System.getenv("STRIMZI_PORT") == null ? DEFAULT_PORT : Integer.parseInt(System.getenv("STRIMZI_PORT"));
+        String topic = System.getenv("STRIMZI_TOPIC") == null ? DEFAULT_TOPIC : System.getenv("STRIMZI_TOPIC");
+        String groupId = System.getenv("STRIMZI_GROUP_ID") == null ? DEFAULT_GROUPID : System.getenv("STRIMZI_GROUP_ID");
+        String clientId = System.getenv("STRIMZI_CLIENT_ID") == null ? DEFAULT_CLIENTID : System.getenv("STRIMZI_CLIENT_ID");
+        Long messageCount = System.getenv("STRIMZI_MESSAGE_COUNT") == null ? DEFAULT_MESSAGES_COUNT : Long.parseLong(System.getenv("STRIMZI_MESSAGE_COUNT"));
+        int pollInterval = System.getenv("STRIMZI_POLL_INTERVAL") == null ? DEFAULT_POLL_INTERVAL : Integer.parseInt(System.getenv("STRIMZI_POLL_INTERVAL"));
+        int pollTimeout = System.getenv("STRIMZI_POLL_TIMEOUT") == null ? DEFAULT_POLL_TIMEOUT : Integer.parseInt(System.getenv("STRIMZI_POLL_TIMEOUT"));
+        TracingSystem tracingSystem = TracingSystem.forValue(System.getenv().getOrDefault("STRIMZI_TRACING_SYSTEM", ""));
+        return new HttpConsumerConfig(hostName, port, topic, groupId, clientId, messageCount, pollInterval, pollTimeout, tracingSystem);
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public long getMessageCount() {
+        return messageCount;
+    }
+
+    public int getPollInterval() {
+        return pollInterval;
+    }
+
+    public int getPollTimeout() {
+        return pollTimeout;
+    }
+
+    public TracingSystem getTracingSystem() {
+        return tracingSystem;
+    }
+
+    @Override
+    public String toString() {
+        return "HttpConsumerConfig{" +
+                "hostName='" + hostName + "'" +
+                ", port=" + port +
+                ", topic=" + topic +
+                ", groupId=" + groupId +
+                ", clientId=" + clientId +
+                ", messageCount=" + messageCount +
+                ", pollInterval=" + pollInterval +
+                ", pollTimeout=" + pollTimeout +
+                ", tracingSystem=" + tracingSystem +
+                "}";
+    }
+}
---
+++ java/http/java-http-producer/src/main/java/HttpProducer.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanBuilder;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.api.trace.StatusCode;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class HttpProducer {
+
+    private static final Logger log = LogManager.getLogger(HttpProducer.class);
+
+    private final HttpProducerConfig config;
+    private int messageSent = 0;
+    private ScheduledExecutorService executorService;
+    private HttpClient httpClient;
+    private URI sendEndpoint;
+    private Tracer tracer;
+
+    public static void main(String[] args) throws InterruptedException, URISyntaxException {
+        HttpProducerConfig config = HttpProducerConfig.fromEnv();
+
+        TracingSystem tracingSystem = config.getTracingSystem();
+        if (tracingSystem != TracingSystem.NONE) {
+            if (tracingSystem == TracingSystem.OPENTELEMETRY) {
+                TracingInitializer.otelInitialize();
+            } else {
+                log.error("Error: STRIMZI_TRACING_SYSTEM {} is not recognized or supported!", config.getTracingSystem());
+            }
+        }
+
+        HttpProducer httpProducer = new HttpProducer(config);
+        httpProducer.run();
+    }
+
+    public HttpProducer(HttpProducerConfig config) throws URISyntaxException {
+        this.config = config;
+        this.executorService = Executors.newSingleThreadScheduledExecutor();
+        this.httpClient = HttpClient.newHttpClient();
+        this.sendEndpoint = new URI("http://" + this.config.getHostName() + ":" + this.config.getPort() + "/topics/" + this.config.getTopic());
+        this.tracer = this.config.getTracingSystem().equals(TracingSystem.NONE) ?
+                OpenTelemetry.noop().getTracer("client-examples") :
+                GlobalOpenTelemetry.getTracer("client-examples");
+    }
+
+    public void run() throws InterruptedException {
+        log.info("Scheduling periodic send: {} messages every {} ms ...", this.config.getMessageCount(), this.config.getDelay());
+        this.executorService.scheduleAtFixedRate(this::scheduledSend, 0, this.config.getDelay(), TimeUnit.MILLISECONDS);
+        this.executorService.awaitTermination(this.config.getDelay() * this.config.getMessageCount() + 60_000L, TimeUnit.MILLISECONDS);
+        log.info("... {} messages sent", this.messageSent);
+    }
+
+    private void scheduledSend() {
+        this.send();
+        if (this.messageSent == this.config.getMessageCount()) {
+            this.executorService.shutdown();
+        }
+    }
+
+    private void send() {
+        try {
+            SpanBuilder spanBuilder = this.tracer.spanBuilder("send-messages");
+            spanBuilder.setSpanKind(SpanKind.CLIENT);
+            spanBuilder.setAttribute(SemanticAttributes.HTTP_METHOD, "POST");
+            spanBuilder.setAttribute(SemanticAttributes.HTTP_URL, this.sendEndpoint.toString());
+            Span span = spanBuilder.startSpan();
+
+            String record = "{\"key\":\"key-" + this.messageSent + "\",\"value\":\"" + this.config.getMessage() + "-" + this.messageSent + "\"}";
+            String records = "{\"records\":[" + record + "]}";
+            log.info("Sending message = {}", record);
+
+            HttpRequest.Builder builder = HttpRequest.newBuilder()
+                    .uri(this.sendEndpoint)
+                    .headers("Content-Type", "application/vnd.kafka.json.v2+json")
+                    .POST(HttpRequest.BodyPublishers.ofString(records));
+
+            try (Scope ignored = span.makeCurrent()) {
+                GlobalOpenTelemetry.getPropagators().getTextMapPropagator().inject(Context.current(), builder, HttpRequest.Builder::setHeader);
+                HttpRequest request = builder.build();
+
+                HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+                span.setAttribute(SemanticAttributes.HTTP_STATUS_CODE, response.statusCode());
+                span.setStatus(response.statusCode() == 200 ? StatusCode.OK : StatusCode.ERROR);
+
+                log.info("code = {}, metadata = {}", response.statusCode(), response.body());
+            } finally {
+                span.end();
+            }
+
+            this.messageSent++;
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
