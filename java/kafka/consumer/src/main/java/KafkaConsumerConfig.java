889e47056074b955d467dc433b11e4a5b09f417e
Owen <owencorrigan76@gmail.com>
2023-03-01 17:13:37 +0000

    Create package in Common module
    Signed-off-by: Owen <owencorrigan76@gmail.com>

--- java/kafka/consumer/src/main/java/KafkaConsumerConfig.java
+++ java/kafka/consumer/src/main/java/KafkaConsumerConfig.java
@@ -3,6 +3,8 @@
  * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
  */
 
+import io.strimzi.common.ConfigUtil;
+
 import java.util.Map;
 import java.util.Properties;
 import java.util.stream.Collectors;
