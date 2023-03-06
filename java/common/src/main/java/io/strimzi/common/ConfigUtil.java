ed4b7e8ef0cae6a6f26a7bf4b5b0276dc39011c3
Owen <owencorrigan76@gmail.com>
2023-03-06 09:51:41 +0000

    Create javadoc in package in Common module
    Signed-off-by: Owen <owencorrigan76@gmail.com>

--- java/common/src/main/java/io/strimzi/common/ConfigUtil.java
+++ java/common/src/main/java/io/strimzi/common/ConfigUtil.java
@@ -4,10 +4,17 @@
  */
 package io.strimzi.common;
 
+/**
+ * Provides utility methods for managing common configuration properties.
+ */
 public class ConfigUtil {
-
+    /**
+     * Converts environment variables into a corresponding property key format.
+     *
+     * @param envVar Name of the environment variable to be converted to property key format.
+     * @return Returns a String which removes a prefix containing '_', converts to lower case and replaces '_' with '.'.
+     */
     public static String convertEnvVarToPropertyKey(String envVar) {
         return envVar.substring(envVar.indexOf("_") + 1).toLowerCase().replace("_", ".");
     }
-
 }
