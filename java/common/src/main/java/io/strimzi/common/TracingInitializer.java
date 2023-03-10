b657af6cee514ae0891976ff6c938c50c166383a
Owen Corrigan <76431349+OwenCorrigan76@users.noreply.github.com>
2023-03-10 12:35:49 +0000

    Write javadocs for new classes and methods (#116)
    * Added javadoc to TracingInitializer and TracingSystem
    
    Signed-off-by: Owen <owencorrigan76@gmail.com>
    
    * Address javadoc comments
    
    Signed-off-by: Owen <owencorrigan76@gmail.com>
    
    * Addess Paolo's nit
    
    Signed-off-by: Owen <owencorrigan76@gmail.com>
    
    ---------
    
    Signed-off-by: Owen <owencorrigan76@gmail.com>

--- java/common/src/main/java/io/strimzi/common/TracingInitializer.java
+++ java/common/src/main/java/io/strimzi/common/TracingInitializer.java
@@ -9,14 +9,29 @@ import io.opentracing.Tracer;
 import io.opentracing.util.GlobalTracer;
 import io.jaegertracing.Configuration;
 
+/**
+ * Provides methods for configuring tracing
+ */
 public class TracingInitializer {
-
+    /**
+     * Initializes Jaeger Tracing by loading the configuration from the environment
+     * and registering the tracer as a global tracer
+     *
+     * @return a Jaeger Tracer object that is initialised from the environment variables
+     */
     public static Tracer jaegerInitialize() {
         Tracer tracer = Configuration.fromEnv().getTracer();
         GlobalTracer.registerIfAbsent(tracer);
         return tracer;
     }
 
+    /**
+     * Initializes OpenTelemetry Tracing by loading the configuration from the environment
+     * and registering the tracer as a global tracer
+     * It sets up the global OpenTelemetry variable with a default SDK instance.
+     *
+     * @see AutoConfiguredOpenTelemetrySdk
+     */
     public static void otelInitialize() {
         AutoConfiguredOpenTelemetrySdk.initialize();
     }
