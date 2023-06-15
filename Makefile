25fdecbb8dd1b376734964b337b7f055361aa2ec
Owen <owencorrigan76@gmail.com>
2023-06-15 14:16:36 +0100

    adding logs
    Signed-off-by: Owen <owencorrigan76@gmail.com>

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 RELEASE_VERSION ?= latest
 
-SUBDIRS=java/common java/kafka/consumer java/kafka/producer java/kafka/streams java/http/java-http-consumer java/http/java-http-producer java/http/vertx/java-http-vertx-consumer java/http/vertx/java-http-vertx-producer python/kafka/consumer python/kafka/producer/
+SUBDIRS=python/kafka/consumer python/kafka/producer/
+
+#SUBDIRS=java/common java/kafka/consumer java/kafka/producer java/kafka/streams java/http/java-http-consumer java/http/java-http-producer java/http/vertx/java-http-vertx-consumer java/http/vertx/java-http-vertx-producer python/kafka/consumer python/kafka/producer/
 DOCKER_TARGETS=docker_build docker_push docker_tag
 
 all: $(SUBDIRS)
--- python/kafka/producer/kafka_producer.py
+++ python/kafka/producer/kafka_producer.py
@@ -1,20 +1,26 @@
+import logging
 import time
 from datetime import datetime
 from confluent_kafka import Producer
 from kafka_producer_config import from_env
 
+logging.basicConfig(format='%(message)s',
+                    level=logging.INFO)
+
+log = logging.getLogger(__name__)
+
 if __name__ == '__main__':
     config = from_env()
-
     producer = Producer(
-        config.properties
+        config.properties,
     )
-    print(f'Config Properties: \n{str(config.properties)}')
+    log.info(f'Config Properties: \n{str(config.properties)}')
 
     while True:
         producer_message = config.message.encode('utf-8')
         producer.produce(config.topic, producer_message)
         producer.flush()
-        print(f'Producing message @ {datetime.now()} | Message = {str(producer_message)}')
+        log.info(f'Producing message @ {datetime.now()} | Message = {str(producer_message)}')
+
         time.sleep(config.delay)
 
