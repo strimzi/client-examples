4e3ece7c39bed15b726c3936fba34a4da763db6a
Owen <owencorrigan76@gmail.com>
2024-09-03 13:02:23 +0100

    reduce duplicaion in config files
    Signed-off-by: Owen <owencorrigan76@gmail.com>

--- java/http/java-http-consumer/src/main/java/HttpConsumerConfig.java
+++ java/http/java-http-consumer/src/main/java/HttpConsumerConfig.java
@@ -2,7 +2,6 @@
  * Copyright Strimzi authors.
  * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
  */
-
 import io.strimzi.common.ConfigUtil;
 import io.strimzi.common.TracingSystem;
 
